195034a1b61842a977b05dfd3a46f17b2026c8db
bad-boy-discord <46648824+bad-boy-discord@users.noreply.github.com>
2021-09-07 14:41:30 +0300

    [Code] First Commit

---
+++ interfaces/Command.ts
@@ -0,0 +1,17 @@
+import { Client, Message, Permissions } from "discord.js";
+
+export interface Command {
+    name: string;
+    description?: string;
+    aliases?: string[];
+    usage?: string;
+    ownerOnly?: boolean;
+    userPerms?: Permissions;
+    botPerms?: Permissions;
+
+    run?: CommandRun;
+}
+
+export interface CommandRun {
+    (client: Client, message: Message, args: string[]): Promise<unknown> | unknown;
+};
